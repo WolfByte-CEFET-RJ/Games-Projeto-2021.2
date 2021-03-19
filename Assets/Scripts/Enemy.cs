121fea6b6a817732be1044d744d8a3e838f60d53
Vinicius <viniciusantos0898@gmail.com>
2021-03-19 15:34:28 -0300

    enemy scripts

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,19 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{
-    public Conductor conductor;
-    
-    public Transform RemovePos;
-    public Transform SpawnPos;
-
-    
+{    
     void Update()
     {   
-        transform.position = Vector2.Lerp(
-        SpawnPos.position,
-        RemovePos.position,
-        (conductor.BeatsShownInAdvance -(conductor.songPosition - conductor.songPositionInBeats)) / conductor.BeatsShownInAdvance
-    );
+        transform.position = new Vector2(-(2 * Time.deltaTime), 0f);
     }
 }
